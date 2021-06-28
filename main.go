ab005c5713625b912403cff6c82298f760a34983
iamargus95 <kamathsuraj95@gmail.com>
2021-06-29 03:06:45 +0530

    ListenandServe implemented

--- main.go
+++ main.go
@@ -1,34 +1,42 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"strconv"
+	"html/template"
+	"net/http"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var tpl *template.Template
+
 func main() {
 	fmt.Println("Hello World!!")
 
-	database, _ := sql.Open("sqlite3", "./mydata.db")
+	// database, _ := sql.Open("sqlite3", "./mydata.db")
 
-	statement1, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, email TEXT, password TEXT)")
-	statement1.Exec()
+	// statement1, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, email TEXT, password TEXT)")
+	// statement1.Exec()
 
-	statement2, _ := database.Prepare("INSERT INTO people (email, password) VALUES (? , ?)")
-	statement2.Exec("email4", "password4") //Placeholder data. Get input from login.html
+	// statement2, _ := database.Prepare("INSERT INTO people (email, password) VALUES (? , ?)")
+	// statement2.Exec("email4", "password4") //Placeholder data. Get input from login.html
 
-	rows, _ := database.Query("SELECT id, email, password FROM people")
+	// rows, _ := database.Query("SELECT id, email, password FROM people")
 
-	var (
-		id       int
-		email    string
-		password string
-	)
+	// var (
+	// 	id       int
+	// 	email    string
+	// 	password string
+	// )
 
-	for rows.Next() {
-		rows.Scan(&id, &email, &password)
-		fmt.Println(strconv.Itoa(id) + ":" + email + " " + password)
-	}
+	http.HandleFunc("/", login)
+	http.ListenAndServe(":8000", nil)
+}
+
+func init() {
+	tpl = template.Must(template.ParseGlob("./login.html"))
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "login.html", nil)
 }
