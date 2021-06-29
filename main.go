d395c97778ed327f1a80306a4d11f79513c53786
iamargus95 <kamathsuraj95@gmail.com>
2021-06-29 16:17:36 +0530

    Added the function for registration page

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 
 	http.HandleFunc("/", login)
 	http.HandleFunc("/login", loginpage)
+	http.HandleFunc("/register", registerpage)
 	http.ListenAndServe(":8000", nil)
 }
 
@@ -40,8 +41,11 @@ func loginpage(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer stmt.Close()
+
 	var password string
+
 	err = stmt.QueryRow(formemail).Scan(&password)
 	if err != nil {
 		log.Fatal(err)
@@ -53,4 +57,35 @@ func loginpage(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("KNOPE")
 	}
 
+}
+
+func registerpage(w http.ResponseWriter, r *http.Request) {
+
+	formemail1 := r.FormValue("email")
+	formpsw1 := r.FormValue("psw")
+
+	database, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer database.Close()
+
+	stmt1, err1 := database.Exec("INSERT INTO users (email, password) VALUES (?, ?)", formemail1, formpsw1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var email string
+
+	err1 = stmt1.QueryRow(formemail1).Scan(&email)
+	if err != nil {
+		log.Fatal(err1)
+	}
+
+	if email == formemail1 {
+		fmt.Println("This email is already in use.")
+	} else {
+		fmt.Println("Successfully created a new account.")
+	}
 }
