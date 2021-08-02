1657a1f55df3a6a16b4eaa57bf63e1cabf2530fa
iamargus95 <kamathsuraj95@gmail.com>
2021-08-02 18:40:50 +0530

    Practicing syntax and reading up imported libraries.

---
+++ Tour_of_Golang/exported-names.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Pi)
+	fmt.Println(math.Phi)
+	fmt.Println(math.E)
+	fmt.Println(math.Sqrt2)
+	fmt.Println(math.SqrtE)
+	fmt.Println(math.SqrtPhi)
+	fmt.Println(math.SqrtPi)
+	fmt.Println(math.Log2E)
+	fmt.Println(math.Log10E)
+	fmt.Println(math.Ln2)
+	fmt.Println(math.Ln10)
+}
