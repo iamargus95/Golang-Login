6d4cf2dd4940fbf4bb11df284e0a653900d9220a
iamargus95 <kamathsuraj95@gmail.com>
2021-08-17 23:25:30 +0530

    code reviewed

--- Assignments/Josephus_Problem/main.go
+++ Assignments/Josephus_Problem/main.go
@@ -6,29 +6,24 @@ import (
 	"strings"
 )
 
-var (
-	N        int //Nunber of soldiers in the Josephus Problem.
-	decimalN int64
-)
-
 func main() {
 
+	var noOfSoldiers int64
 	fmt.Printf("\nEnter the number of people you want for the josephus problem: ")
 
-	fmt.Scanln(&N)
-	decimalN = int64(N)
+	fmt.Scanln(&noOfSoldiers)
 
-	josephusSimulation(int64(decimalN))
+	josephusSimulation(noOfSoldiers)
 }
 
-func josephusSimulation(decimalN int64) {
+func josephusSimulation(noOfSoldiers int64) {
 
-	binarySoldier := strconv.FormatInt(decimalN, 2)
+	binarySoldier := strconv.FormatInt(noOfSoldiers, 2)
+	fmt.Println(binarySoldier)
 
 	binarySoldierArray := strings.Split(binarySoldier, "")
 
 	firstElement, _ := strconv.Atoi(binarySoldierArray[0])
-	toBeMoved := []int{firstElement}
 
 	mutatedBinarySoldierArray := []int{}
 	for _, i := range binarySoldierArray {
@@ -43,7 +38,7 @@ func josephusSimulation(decimalN int64) {
 	len := len(mutatedBinarySoldierArray)
 
 	restOfTheArray := mutatedBinarySoldierArray[1:len]
-	survivorSoldier := append(restOfTheArray, toBeMoved...)
+	survivorSoldier := append(restOfTheArray, firstElement)
 
 	survivorSoldierString := []string{}
 	for _, i := range survivorSoldier {
