48780e6430808de7e055f993417eb9b74750ed4c
iamargus95 <kamathsuraj95@gmail.com>
2021-08-22 12:46:11 +0530

    Initial Json unmarshall

--- Assignments/GitHub_REST_API_consumer/go.mod
+++ Assignments/GitHub_REST_API_consumer/go.mod
@@ -1,10 +1,3 @@
 module iamargus95/Learning-Golang/GitHub_REST_API_consumer
 
 go 1.17
-
-require github.com/google/go-github/v38 v38.1.0
-
-require (
-	github.com/google/go-querystring v1.0.0 // indirect
-	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
-)
--- Assignments/GitHub_REST_API_consumer/main.go
+++ Assignments/GitHub_REST_API_consumer/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"text/template"
-
-	"github.com/google/go-github/v38/github"
 )
 
 var (
@@ -19,6 +19,41 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
+type Userinfo struct {
+	Login               string
+	Id                  int
+	Node_id             string
+	Avatar_url          string
+	Gravatar_id         string
+	Url                 string
+	Html_url            string
+	Followers_url       string
+	Following_url       string
+	Gists_url           string
+	Starred_url         string
+	Subscriptions_url   string
+	Organizations_url   string
+	Repos_url           string
+	Events_url          string
+	Received_events_url string
+	Type                string
+	Site_admin          bool
+	Name                string
+	Company             string
+	Blog                string
+	Location            string
+	Email               string
+	Hireable            bool
+	Bio                 string
+	Twitter_username    string
+	Public_repos        int
+	Public_gists        int
+	Followers           int
+	Following           int
+	Created_at          string
+	Updated_at          string
+}
+
 func userName(w http.ResponseWriter, r *http.Request) {
 
 	tpl = template.Must(template.ParseGlob("./index.html"))
@@ -26,7 +61,29 @@ func userName(w http.ResponseWriter, r *http.Request) {
 
 	formUsername = r.FormValue("username")
 
-	fmt.Println(formUsername)
+	url := "https://api.github.com/users/" + formUsername
 
-	client := github.NewClient(nil)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var users Userinfo
+	json.Unmarshal([]byte(body), &users)
+	// fmt.Println(users)
+	fmt.Printf("Name: %s\nLogin: %s\nURL: %s\nBio: %s\nPublic Repositories: %d\nFollowers: %d\nFollowing: %d\n", users.Name, users.Login, users.Url, users.Bio, users.Public_repos, users.Followers, users.Following)
 }
