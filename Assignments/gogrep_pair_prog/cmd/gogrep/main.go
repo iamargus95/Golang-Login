ef380efcb3419c1765e277d0e9d48129b03211d9
iamargus95 <kamathsuraj95@gmail.com>
2021-08-30 15:57:30 +0530

    cmd restucture

---
+++ Assignments/gogrep_pair_prog/cmd/gogrep/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	var after int
+	flag.IntVar(&after, "A", 2, "number of lines after the match.")
+
+	var before int
+	flag.IntVar(&before, "B", 2, "number of lines before the match.")
+
+	var around int
+	flag.IntVar(&around, "C", 2, "number of lines before and after the match.")
+
+	var recursive bool
+	flag.BoolVar(&recursive, "r", true, "List files in a directory recursively.")
+
+	flag.Parse()
+
+	pattern := PatternString()
+	path := os.Args[2]
+	fmt.Printf(SearchString(path, pattern) + "\n")
+}
--- Assignments/gogrep_pair_prog/gogrep_test.go
+++ Assignments/gogrep_pair_prog/gogrep_test.go
@@ -1,4 +1,4 @@
-package main
+package gogrep
 
 import (
 	"os"
