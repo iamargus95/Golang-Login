df3b39f634dddb3d539dff4ac6b69a86964c139a
iamargus95 <kamathsuraj95@gmail.com>
2021-08-30 12:38:33 +0530

    added regexp to search patterns in one file

--- Assignments/gogrep_pair_prog/main.go
+++ Assignments/gogrep_pair_prog/main.go
@@ -1,19 +1,83 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
+	"regexp"
 )
 
-func walkpath(path string, f os.FileInfo, err error) error {
-	fmt.Printf("%s with %d bytes\n", path, f.Size())
-	return nil
+func main() {
+
+	var after int
+	flag.IntVar(&after, "A", 2, "number of lines after the match.")
+
+	var before int
+	flag.IntVar(&before, "B", 2, "number of lines before the match.")
+
+	var around int
+	flag.IntVar(&around, "C", 2, "number of lines before and after the match.")
+
+	var recursive bool
+	flag.BoolVar(&recursive, "r", true, "List files in a directory recursively.")
+
+	var insensitive bool
+	flag.BoolVar(&insensitive, "i", true, "List files in a directory recursively.")
+
+	flag.Parse()
+
+	pattern := os.Args[1]
+
+	searchString(os.Args[2], pattern)
 }
 
-func main() {
-	flag.Parse()
-	root := flag.Arg(0) // 1st argument is the directory location
-	filepath.Walk(root, walkpath)
+func walkPathStore() []string {
+
+	inputFiles := []string{}
+	err := filepath.Walk(os.Args[1], func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			inputFiles = append(inputFiles, path)
+		}
+		return nil
+	})
+	if err != nil {
+		log.Println(err)
+	}
+	return inputFiles
+}
+
+func searchString(path, pattern string) {
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	r, err := regexp.Compile(pattern) // this can also be a regex
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for scanner.Scan() {
+		if r.MatchString(scanner.Text()) {
+			fmt.Println(scanner.Text())
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func patternString() string {
+	return (os.Args[1])
 }
